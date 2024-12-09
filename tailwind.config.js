84ad9d1dc4c2a255dff0acd69696a32d97210a74
khoa <khoa2003httn@gmail.com>
2024-12-09 08:49:59 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Thêm đường dẫn tới các tệp cần quét
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
